f5a61de05ed089bc9c597bca49ad1a8879a2fb43
M A S T E R <kiril.svetoslavov.kirilov@gmail.com>
2022-11-05 23:31:40 +0200

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("soft_uni");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+
+        entityManager.getTransaction().begin();
+
+
+
+
+
+
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+
     }
 }
